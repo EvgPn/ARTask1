58de2db5907566f5a4a8a3b3ef397d511333007e
Evg Pn <ye.ponomarenko@gmail.com>
2020-05-08 21:28:55 +0300

    feat(project) add buttons
    -add play, stop and next video buttons
    -add NextVideoScript with list of url's of videos

---
+++ ARTest1/Assets/Scripts/NextVideoScripts.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class NextVideoScripts : MonoBehaviour
+{
+	[SerializeField] private List<string> _urlOfVideos = new List<string>();
+	private int _indexOfCurrentURL = 0;
+
+	public void PlayNextVideo()
+	{
+		_indexOfCurrentURL++;
+
+		if (_indexOfCurrentURL >= _urlOfVideos.Count)
+		{
+			_indexOfCurrentURL = 0;
+		}
+
+		gameObject.GetComponent<VideoPlayer>().url = _urlOfVideos[_indexOfCurrentURL];
+		gameObject.GetComponent<VideoPlayer>().Play();
+	}
+}
